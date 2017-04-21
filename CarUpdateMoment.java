8b36a9e110af746fd9980f7181d91c70ad8d3f7b
sellasanchana <sellasanchana@gmail.com>
2017-04-21 18:47:59 +0530

    Create CarUpdateMoment.java

---
+++ CarUpdateMoment.java
@@ -0,0 +1,23 @@
+/**
+ * A basic event that is sent off to registered listeners
+ * @
+ *
+ * PUBLIC FEATURES:
+ * // Constructors
+ *    public CarUpdateEvent(Object source)
+ *
+ * // Methods
+ *
+ * COLLABORATORS:
+ *
+ */
+public class CarUpdateEvent extends java.util.EventObject
+{
+	/**
+	 * @param source source object of event
+	 */
+	public CarUpdateEvent(Object source)
+	{
+		super(source);
+	}
+}
