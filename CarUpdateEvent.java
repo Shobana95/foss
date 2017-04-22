6eaeda4bb9231554053a602ab2e5114040fea419
sellasanchana <sellasanchana@gmail.com>
2017-04-22 14:19:32 +0530

    Create CarUpdateEvent.java

---
+++ CarUpdateEvent.java
@@ -0,0 +1,18 @@
+/**
+ * A basic event that is sent off to registered listeners
+ *
+ *
+ * PUBLIC FEATURES:
+ * // Constructors
+ *    public CarUpdateEvent(Object source)
+ */
+public class CarUpdateEvent extends java.util.EventObject
+{
+	/**
+	 * @param source source object of event
+	 */
+	public CarUpdateEvent(Object source)
+	{
+		super(source);
+	}
+}
